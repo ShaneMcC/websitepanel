16e2108fef439d57f331e0fceb40d5cfba746624
feodorf <feodorf@a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8>
2010-04-19 19:02:14 +0000

    Tag WebsitePanel.VmConfig 1.0 release
    git-svn-id: https://websitepanel.svn.sourceforge.net/svnroot/websitepanel@10 a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8

---
+++ WebsitePanel.VmConfig/Releases/1.0/Sources/WebsitePanel.VmConfig/ProjectInstaller.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WebsitePanel.VmConfig
+{
+	partial class ProjectInstaller
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.serviceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller
+            // 
+            this.serviceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller.Password = null;
+            this.serviceProcessInstaller.Username = null;
+            // 
+            // serviceInstaller
+            // 
+            this.serviceInstaller.Description = "Performs provisioning operations in the quest operating system.";
+            this.serviceInstaller.DisplayName = "WebsitePanel Virtual Machine Configuration Service";
+            this.serviceInstaller.ServiceName = "WSPVmConfig";
+            this.serviceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+
+            this.serviceInstaller.ServicesDependedOn = new string[] { "vmickvpexchange" };
+
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller,
+            this.serviceInstaller});
+
+		}
+
+		#endregion
+
+		private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller;
+		private System.ServiceProcess.ServiceInstaller serviceInstaller;
+	}
+}
