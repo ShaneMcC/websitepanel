519ec3a8d5c378d456f7eb2a86a0ed9f318e6bf3
feodorf <feodorf@a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8>
2010-04-19 19:06:17 +0000

    Tag WebsitePanel.HyperV.Utils 1.0 release
    git-svn-id: https://websitepanel.svn.sourceforge.net/svnroot/websitepanel@11 a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8

---
+++ WebsitePanel.HyperV.Utils/Releases/1.0/Sources/WebsitePanel.HyperV.Utils/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Hyper-V Utilities")]
+[assembly: AssemblyDescription("Utility to work with Hyper-V virtual resources.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SMB SAAS Systems Inc.")]
+[assembly: AssemblyProduct("VmUtils")]
+[assembly: AssemblyCopyright("Copyright © SMB SAAS Systems Inc. 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c72a836b-0d6e-482f-afba-8c03964d9933")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
