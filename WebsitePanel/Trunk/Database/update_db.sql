83acea3cd20e96c2e54b9dd01430cb7502f6952a
feodorf <feodorf@a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8>
2010-07-15 01:52:52 +0000

    Preparing ES solution for 1.0.1 build
    git-svn-id: https://websitepanel.svn.sourceforge.net/svnroot/websitepanel@69 a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8

--- WebsitePanel/Trunk/Database/update_db.sql
+++ WebsitePanel/Trunk/Database/update_db.sql
@@ -1,3 +1,6 @@
+﻿USE [WebsitePanel_build]
+GO
+
 -- update zone template for "WEB" services (IIS 6 and IIS 7) to support "Al unassigned"
 update [dbo].[ResourceGroupDnsRecords]
 set RecordData = '[IP]'
