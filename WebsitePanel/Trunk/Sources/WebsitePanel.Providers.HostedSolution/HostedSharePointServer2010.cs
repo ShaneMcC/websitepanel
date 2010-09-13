3be6346a364d25a9552d6b370feec01c00c0e27c
feodorf <feodorf@a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8>
2010-09-13 23:11:06 +0000

    git-svn-id: https://websitepanel.svn.sourceforge.net/svnroot/websitepanel@96 a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8

---
+++ WebsitePanel/Trunk/Sources/WebsitePanel.Providers.HostedSolution/HostedSharePointServer2010.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebsitePanel.Providers.HostedSolution
+{
+    public class HostedSharePointServer2010 : HostedSharePointServer
+    {
+        public HostedSharePointServer2010()
+        {
+            this.Wss3RegistryKey = @"SOFTWARE\Microsoft\Shared Tools\Web Server Extensions\14.0";
+            this.Wss3Registry32Key = @"SOFTWARE\Wow6432Node\Microsoft\Shared Tools\Web Server Extensions\14.0";
+            this.LanguagePacksPath = @"%commonprogramfiles%\microsoft shared\Web Server Extensions\14\HCCab\";
+        }
+    }
+}
---
+++ WebsitePanel/Trunk/Sources/WebsitePanel.Server/ExchangeServerHostedEdition.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="ExchangeServerHostedEdition.asmx.cs" Class="WebsitePanel.Server.ExchangeServerHostedEdition" %>
