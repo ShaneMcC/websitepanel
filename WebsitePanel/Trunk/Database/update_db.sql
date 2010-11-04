e1065180148862455c3b35ec8946d7b639573857
ptsurbeleu <ptsurbeleu@a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8>
2010-11-04 19:02:33 +0000

    Added instructions to update SmarterStats provider display name (6.x version support); Renamed Exchange 2010 SP1 provider;
    git-svn-id: https://websitepanel.svn.sourceforge.net/svnroot/websitepanel@120 a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8

--- WebsitePanel/Trunk/Database/update_db.sql
+++ WebsitePanel/Trunk/Database/update_db.sql
@@ -2,9 +2,31 @@
 GO
 
 -- update database version
-declare @build_version nvarchar(10), @build_date datetime
-set @build_version = '1.1.0.0'
-set @build_date = '09/22/2010'
-IF NOT EXISTS (SELECT * FROM dbo.Versions WHERE DatabaseVersion = @build_version)
-INSERT INTO [Versions] ([DatabaseVersion], [BuildDate]) VALUES (@build_version, @build_date)
+DECLARE @build_version nvarchar(10), @build_date datetime
+SET @build_version = '1.1.0.0'
+SET @build_date = '09/22/2010'
+
+IF NOT EXISTS (SELECT * FROM [dbo].[Versions] WHERE [DatabaseVersion] = @build_version)
+BEGIN
+	INSERT INTO [Versions] ([DatabaseVersion], [BuildDate]) VALUES (@build_version, @build_date)
+END
+GO
+
+-- Update SmarterStats service provider display name
+IF EXISTS (SELECT * FROM [dbo].[Providers] WHERE [ProviderID] = 62)
+BEGIN
+	UPDATE [dbo].[Providers] SET [DisplayName] = N'SmarterStats 5.x-6.x' WHERE [ProviderID] = 62
+END
+GO
+
+IF EXISTS (SELECT * FROM [dbo].[ResourceGroups] WHERE [GroupID] = 33)
+BEGIN
+	UPDATE [dbo].[ResourceGroups] SET [GroupName] = N'ExchangeHostingMode' WHERE [GroupID] = 33
+END
+GO
+
+IF EXISTS (SELECT * FROM [dbo].[Providers] WHERE [ProviderID] = 207)
+BEGIN
+	UPDATE [dbo].[Providers] SET [DisplayName] = N'Exchange Server 2010 SP1 Hosting Mode' WHERE [ProviderID] = 207
+END
 GO
