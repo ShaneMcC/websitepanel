4804d01d1779dffac47eb03c3c8bb27a5927e3ca
ptsurbeleu <ptsurbeleu@a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8>
2010-11-17 03:59:55 +0000

    Upgraded sources version up to 1.1.0.
    git-svn-id: https://websitepanel.svn.sourceforge.net/svnroot/websitepanel@134 a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8

--- WebsitePanel/Trunk/Database/update_db.sql
+++ WebsitePanel/Trunk/Database/update_db.sql
@@ -3,8 +3,8 @@ GO
 
 -- update database version
 DECLARE @build_version nvarchar(10), @build_date datetime
-SET @build_version = '1.1.0.0'
-SET @build_date = '09/22/2010'
+SET @build_version = '1.1.0.9'
+SET @build_date = '11/16/2010'
 
 IF NOT EXISTS (SELECT * FROM [dbo].[Versions] WHERE [DatabaseVersion] = @build_version)
 BEGIN
@@ -16,12 +16,6 @@ GO
 IF EXISTS (SELECT * FROM [dbo].[Providers] WHERE [ProviderID] = 62)
 BEGIN
 	UPDATE [dbo].[Providers] SET [DisplayName] = N'SmarterStats 5.x-6.x' WHERE [ProviderID] = 62
-END
-GO
-
-IF EXISTS (SELECT * FROM [dbo].[ResourceGroups] WHERE [GroupID] = 33)
-BEGIN
-	UPDATE [dbo].[ResourceGroups] SET [GroupName] = N'ExchangeHostingMode' WHERE [GroupID] = 33
 END
 GO
 
