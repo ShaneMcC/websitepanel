887a465bee199ade95d909c951d2d26dcf61dcab
ptsurbeleu <ptsurbeleu@a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8>
2010-12-07 19:09:02 +0000

    Refactoring
    git-svn-id: https://websitepanel.svn.sourceforge.net/svnroot/websitepanel@152 a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8

---
+++ WebsitePanel.Installer/Branches/2.0/Trunk/Sources/WebsitePanel.Installer.Core/ServiceProviderProxy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebsitePanel.Installer.Configuration;
+using System.Net;
+using WebsitePanel.Installer.Services;
+
+namespace WebsitePanel.Installer.Core
+{
+	public class ServiceProviderProxy
+	{
+		public static InstallerService GetInstallerWebService()
+		{
+			var webService = new InstallerService();
+
+			string url = AppConfigManager.AppConfiguration.GetStringSetting(ConfigKeys.Web_Service);
+			if (!String.IsNullOrEmpty(url))
+			{
+				webService.Url = url;
+			}
+			else
+			{
+				webService.Url = "http://www.websitepanel.net/Services/InstallerService.asmx";
+			}
+
+			// check if we need to add a proxy to access Internet
+			bool useProxy = AppConfigManager.AppConfiguration.GetBooleanSetting(ConfigKeys.Web_Proxy_UseProxy);
+			if (useProxy)
+			{
+				string proxyServer = AppConfigManager.AppConfiguration.Settings[ConfigKeys.Web_Proxy_Address].Value;
+				if (!String.IsNullOrEmpty(proxyServer))
+				{
+					IWebProxy proxy = new WebProxy(proxyServer);
+					string proxyUsername = AppConfigManager.AppConfiguration.Settings[ConfigKeys.Web_Proxy_UserName].Value;
+					string proxyPassword = AppConfigManager.AppConfiguration.Settings[ConfigKeys.Web_Proxy_Password].Value;
+					if (!String.IsNullOrEmpty(proxyUsername))
+						proxy.Credentials = new NetworkCredential(proxyUsername, proxyPassword);
+					webService.Proxy = proxy;
+				}
+			}
+
+			return webService;
+		}
+	}
+}
