9a9a150baa4892d36b6c70764c3ed8160c2b5aa0
ptsurbeleu <ptsurbeleu@a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8>
2011-04-12 02:03:40 +0000

    What's new: - Updated build version in build.xml; - Added minor corrections to the database update script;
    git-svn-id: https://websitepanel.svn.sourceforge.net/svnroot/websitepanel@217 a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8

--- WebsitePanel/Releases/1.1.2/Database/update_db.sql
+++ WebsitePanel/Releases/1.1.2/Database/update_db.sql
@@ -8,7 +8,7 @@ SET @build_date = '${release.date}T00:00:00' -- ISO 8601 Format (YYYY-MM-DDTHH:M
 
 IF NOT EXISTS (SELECT * FROM [dbo].[Versions] WHERE [DatabaseVersion] = @build_version)
 BEGIN
-	INSERT INTO [Versions] ([DatabaseVersion], [BuildDate]) VALUES (@build_version, @build_date)
+	INSERT [dbo].[Versions] ([DatabaseVersion], [BuildDate]) VALUES (@build_version, @build_date)
 END
 GO
 
