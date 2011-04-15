45775e31cad6b4e9597a1fe98b3ed4a08d16adfc
ptsurbeleu <ptsurbeleu@a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8>
2011-04-15 01:34:14 +0000

    What's new: - Version 1.1.3 branch;
    git-svn-id: https://websitepanel.svn.sourceforge.net/svnroot/websitepanel@222 a9e2437e-3d8e-4bcf-a568-cffb5f4cf1e8

---
+++ WebsitePanel.Installer/Releases/1.1.3/Sources/WebsitePanel.Setup/EnterpriseServer10.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebsitePanel.Setup
+{
+	/// <summary>
+	/// Release 1.1.2
+	/// </summary>
+	public class EnterpriseServer112 : EnterpriseServer
+	{
+		public static new object Install(object obj)
+		{
+			return EnterpriseServer.InstallBase(obj, "1.1.0");
+		}
+
+		public static new DialogResult Uninstall(object obj)
+		{
+			return EnterpriseServer.Uninstall(obj);
+		}
+
+		public static new DialogResult Setup(object obj)
+		{
+			return EnterpriseServer.Setup(obj);
+		}
+
+		public static new DialogResult Update(object obj)
+		{
+			return UpdateBase(obj, "1.1.0", "1.1.0", true);
+		}
+	}
+
+	/// <summary>
+	/// Release 1.1.1
+	/// </summary>
+	public class EnterpriseServer111 : EnterpriseServer
+	{
+		public static new object Install(object obj)
+		{
+			return EnterpriseServer.InstallBase(obj, "1.1.0");
+		}
+
+		public static new DialogResult Uninstall(object obj)
+		{
+			return EnterpriseServer.Uninstall(obj);
+		}
+
+		public static new DialogResult Setup(object obj)
+		{
+			return EnterpriseServer.Setup(obj);
+		}
+
+		public static new DialogResult Update(object obj)
+		{
+			return UpdateBase(obj, "1.1.0", "1.1.0", true);
+		}
+	}
+
+	/// <summary>
+	/// Release 1.1.0
+	/// </summary>
+	public class EnterpriseServer110 : EnterpriseServer
+	{
+		public static new object Install(object obj)
+		{
+			return EnterpriseServer.InstallBase(obj, "1.1.0");
+		}
+
+		public static new DialogResult Uninstall(object obj)
+		{
+			return EnterpriseServer.Uninstall(obj);
+		}
+
+		public static new DialogResult Setup(object obj)
+		{
+			return EnterpriseServer.Setup(obj);
+		}
+
+		public static new DialogResult Update(object obj)
+		{
+			return UpdateBase(obj, "1.1.0", "1.0.2", true);
+		}
+	}
+
+    /// Release 1.0.2
+    /// </summary>
+    public class EnterpriseServer102 : EnterpriseServer101
+    {
+        public static new object Install(object obj)
+        {
+            return EnterpriseServer101.InstallBase(obj, "1.0.0");
+        }
+
+        public static new DialogResult Uninstall(object obj)
+        {
+            return EnterpriseServer101.Uninstall(obj);
+        }
+
+        public static new DialogResult Setup(object obj)
+        {
+            return EnterpriseServer101.Setup(obj);
+        }
+
+        public static new DialogResult Update(object obj)
+        {
+            return UpdateBase(obj, "1.0.0", "1.0.1", true);
+        }
+    }
+
+    /// <summary>
+    /// Release 1.0.1
+    /// </summary>
+    public class EnterpriseServer101 : EnterpriseServer10
+    {
+        public static new object Install(object obj)
+        {
+            return EnterpriseServer10.InstallBase(obj, "1.0.0");
+        }
+
+        public static new DialogResult Uninstall(object obj)
+        {
+            return EnterpriseServer10.Uninstall(obj);
+        }
+
+        public static new DialogResult Setup(object obj)
+        {
+            return EnterpriseServer10.Setup(obj);
+        }
+
+        public static new DialogResult Update(object obj)
+        {
+            return UpdateBase(obj, "1.0.0", "1.0", true);
+        }
+    }
+
+    /// <summary>
+    /// Release 1.0
+    /// </summary>
+    public class EnterpriseServer10 : EnterpriseServer
+    {
+        public static new object Install(object obj)
+        {
+            return EnterpriseServer.InstallBase(obj, "1.0.0");
+        }
+
+        public static new DialogResult Uninstall(object obj)
+        {
+            return EnterpriseServer.Uninstall(obj);
+        }
+
+        public static new DialogResult Setup(object obj)
+        {
+            return EnterpriseServer.Setup(obj);
+        }
+    }
+}
---
+++ WebsitePanel.Installer/Releases/1.1.3/Sources/WebsitePanel.Setup/StandaloneServerSetup10.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebsitePanel.Setup
+{
+	/// <summary>
+	/// Release 1.1.1
+	/// </summary>
+	public class StandaloneServerSetup111 : StandaloneServerSetup
+	{
+		public static new object Install(object obj)
+		{
+			return StandaloneServerSetup.InstallBase(obj, "1.1.0");
+		}
+	}
+
+	/// <summary>
+	/// Release 1.1.2
+	/// </summary>
+	public class StandaloneServerSetup112 : StandaloneServerSetup
+	{
+		public static new object Install(object obj)
+		{
+			return StandaloneServerSetup.InstallBase(obj, "1.1.0");
+		}
+	}
+
+	/// <summary>
+	/// Release 1.1.0
+	/// </summary>
+	public class StandaloneServerSetup110 : StandaloneServerSetup
+	{
+		public static new object Install(object obj)
+		{
+			return StandaloneServerSetup.InstallBase(obj, "1.1.0");
+		}
+	}
+
+    /// <summary>
+    /// Release 1.0.2
+    /// </summary>
+    public class StandaloneServerSetup102 : StandaloneServerSetup101
+    {
+		public static new object Install(object obj)
+        {
+            return StandaloneServerSetup.InstallBase(obj, "1.0.0");
+        }
+    }
+
+    /// <summary>
+    /// Release 1.0.1
+    /// </summary>
+    public class StandaloneServerSetup101 : StandaloneServerSetup
+    {
+		public static new object Install(object obj)
+        {
+            return StandaloneServerSetup.InstallBase(obj, "1.0.0");
+        }
+    }
+
+    /// <summary>
+    /// Release 1.0
+    /// </summary>
+    public class StandaloneServerSetup10 : StandaloneServerSetup
+    {
+        public static new object Install(object obj)
+        {
+            return StandaloneServerSetup.InstallBase(obj, "1.0.0");
+        }
+    }
+}
